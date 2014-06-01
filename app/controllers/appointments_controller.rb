4852c07e028bd9be752244a0f86c916490a8bd94
Michael Scriven <bigscriv03@gmail.com>
2014-06-01 14:52:28 +0000

    Added methods to appointments controller and associated code

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,4 +2,25 @@ class AppointmentsController < ApplicationController
   def index
     @patient = Patient.find(params[:patient_id])
   end
+  
+  def new
+    @patient = Patient.find(params[:patient_id])
+    @appointment = @patient.appointments.new
+  end
+  
+  def create
+     @patient = Patient.find(params[:patient_id])
+    @appointment = @patient.appointments.new(appointment_params)
+    if @appointment.save
+      flash[:success] = "Added new appointment."
+      redirect_to patient_appointment_path
+    else
+      flash[:error] = "There was a problem adding that appointment."
+      render action: :new
+    end
+  end
+  
+  private
+  def appointment_params
+    params[:appointment].permit(:hour, :date)
 end
