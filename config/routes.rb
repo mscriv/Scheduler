0ed6479749b769384898aa7f27ff951faedade79
Michael Scriven <bigscriv03@gmail.com>
2014-06-05 00:10:33 +0000

    Update to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Scheduler::Application.routes.draw do
   resources :patients do
     resources :appointments
   end
-  root 'patients#index'
+  root 'patients#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
